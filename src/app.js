973f2948ec85ba26d8494b36cfbe838e527f7998
Uder <t316360@tjdft.jus.br>
2024-12-01 22:30:11 -0300

    mandando para o git

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.use('/api/users', require('./routes/userRoutes'));
+
+// MongoDB Connection
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('MongoDB connection error:', err));
+
+module.exports = app;
